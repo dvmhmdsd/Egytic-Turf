3d96c85d43a25a4c353136a1cb6f809b44dd0c01
mohamedsaad <mahameds19999@gmail.com>
2019-11-20 11:54:56 +0200

    edit the structure of modules

--- src/index.js
+++ src/index.js
@@ -2,18 +2,10 @@
 import reactor from "./core/Reactor";
 
 // get all the modules' service providers
-import HomeModule from "./modules/home/service-provider";
-import AboutModule from "./modules/about/service-provider";
-import GalleryModule from "./modules/gallery/service-provider";
-import ContactModule from "./modules/contact/service-provider";
+import AppModule from "./modules/app/service-provider";
 
 // Register all the modules
-reactor.registerServiceProviders([
-  HomeModule,
-  AboutModule,
-  GalleryModule,
-  ContactModule
-]);
+reactor.registerServiceProviders([AppModule]);
 
 // Bootstrap the application
 reactor.react();
